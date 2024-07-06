039533b0e3617aa3835252cfdfe03d9f48daa260
Michael Volk <volk0026@algonquinlive.com>
2024-07-06 18:22:22 -0400

    Created columns

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+document.getElementById("items").style.columns = "100px 3";
 const items=[128512, 128521, 128533, 128550, 128552, 128556, 128562, 128586, 129303, 129315, 129323, 129488]
 const $items=document.getElementById('items')
 const html=[]
